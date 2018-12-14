e58d4640be6a87833ba8fd698a86f4c33ab6eb85
Cass <cnellis@alumni.unc.edu>
2018-12-14 14:34:44 -0500

    corrected files

--- app.py
+++ app.py
@@ -1,44 +1,54 @@
-# import dependencies 
 import os
+
 import pandas as pd
 import numpy as np
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Database Setup 
+
+#################################################
+# Database Setup
+#################################################
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
+
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(db.engine, reflect=True)
+
 # Save references to each table
 Samples_Metadata = Base.classes.sample_metadata
 Samples = Base.classes.samples
 
-# Define Index Route 
+
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
 
-# Define Names Route to return a list of the sample names 
+
 @app.route("/names")
 def names():
     """Return a list of sample names."""
+
     # Use Pandas to perform the sql query
     stmt = db.session.query(Samples).statement
     df = pd.read_sql_query(stmt, db.session.bind)
+
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns)[2:])
 
-# Define Metadata for a Sample Route
+
 @app.route("/metadata/<sample>")
 def sample_metadata(sample):
     """Return the MetaData for a given sample."""
@@ -51,7 +61,9 @@ def sample_metadata(sample):
         Samples_Metadata.BBTYPE,
         Samples_Metadata.WFREQ,
     ]
+
     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+
     # Create a dictionary entry for each row of metadata information
     sample_metadata = {}
     for result in results:
@@ -62,16 +74,19 @@ def sample_metadata(sample):
         sample_metadata["LOCATION"] = result[4]
         sample_metadata["BBTYPE"] = result[5]
         sample_metadata["WFREQ"] = result[6]
+
     print(sample_metadata)
     return jsonify(sample_metadata)
 
-# Define Samples Route that returns values for given sample 
+
 @app.route("/samples/<sample>")
 def samples(sample):
     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
     stmt = db.session.query(Samples).statement
     df = pd.read_sql_query(stmt, db.session.bind)
-    # Filter the data based on the sample number and only keep rows with values above 1
+
+    # Filter the data based on the sample number and
+    # only keep rows with values above 1
     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
     # Format the data to send as json
     data = {
@@ -80,6 +95,7 @@ def samples(sample):
         "otu_labels": sample_data.otu_label.tolist(),
     }
     return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run()
